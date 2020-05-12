4a64f1ae35e8088f7ccb76f2de568a1870c6f9da
Fermin <fermin.hdez@gmail.com>
2020-05-12 13:37:51 +0200

    order directory and add oo examples

---
+++ ruby/oop.rb
@@ -0,0 +1,36 @@
+require_relative('users_bcrypt')
+class Student
+  include Crud
+  attr_accessor :first_name, :last_name, :email, :username, :password
+ 
+  def initialize(firstname, lastname, username, email, password)
+    @first_name = firstname
+    @last_name = lastname
+    @username = username
+    @email = email
+    @password = password
+  end
+ 
+  def to_s
+    "First name: #{@first_name}, Last name: #{@last_name}, Username: #{@username},
+                  email address: #{@email}"
+  end
+
+  def create_hash_password(password)
+
+  end
+ 
+end
+ 
+mashrur = Student.new("Mashrur", "Hossain", "mashrur1", "mashrur@example.com",
+                      "password1")
+john = Student.new("John", "Doe", "john1", "john1@example.com",
+                      "password2")
+puts mashrur
+puts john
+mashrur.last_name = john.last_name
+puts "Mashrur is altered"
+puts mashrur
+hashed_password = mashrur.create_hash_digest(mashrur.password)
+
+puts hashed_password
